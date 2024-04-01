9cf9a172ad620be7bc1894c7fe4fdae38818decb
Bryan Anthony Garcia <anthonyarenas707@gmail.com>
2024-04-01 19:59:30 +1100

    Add project files.

---
+++ ServiceBusDemo.Sender/Program.cs
@@ -0,0 +1,47 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace QueueSender
+{
+    class Program
+    {
+        static string connectionString = "<connection string here>";
+        static string queueName = "messages";
+
+        static ServiceBusClient client;
+        static ServiceBusSender sender;
+        private const int numOfMessages = 3;
+
+        static async Task Main()
+        {
+            var clientOptions = new ServiceBusClientOptions() { TransportType = ServiceBusTransportType.AmqpWebSockets };
+            client = new ServiceBusClient(connectionString, clientOptions);
+            sender = client.CreateSender(queueName);
+
+            using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+            for (int i = 1; i <= numOfMessages; i++)
+            {
+                if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+                {
+                    throw new Exception($"The message {i} is too large to fit in the batch.");
+                }
+            }
+
+            try
+            {
+                await sender.SendMessagesAsync(messageBatch);
+                Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+            }
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up.
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
+
+            Console.WriteLine("Press any key to end the application");
+            Console.ReadKey();
+        }
+    }
+}
